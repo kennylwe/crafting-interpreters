c77f342c8d70be5ae457eaffdbca0d16f94644c3
kenny <kenjlee09@gmail.com>
2026-04-26 14:37:31 -0700

    hi

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,11 +1,8 @@
-
 type lexeme =
   | ID of string
   | NUMBER of int
   | STAR
   | PLUS
-  | MINUS
-  | DIVIDE
   | IF
   | THEN
   | ELSE
@@ -18,24 +15,33 @@ type lexeme =
 
 type sexp = Atom of lexeme | List of sexp list
 
-let rec sexp_of_lexeme_list (lexeme : lexeme list) : sexp * lexeme list = 
+let rec sexp_of_lexeme_list (lexemes : lexeme list) : sexp * lexeme list =
   match lexemes with
-  | LPAREN :: tl -> 
-    let rec sexp_until_rparen tl =
-      match tl with
-      | [] -> failwith "empty list :("
-      | RPAREN :: tl -> (List (List.rev (acc)), tl)
-      | _ -> 
-        let (sexp, tl') = sexp_until_rparen [] tl in
-        sexp_until_rparen (sexp :: acc) tl'
-    in
-    sexp_until_rparen [] tl
-
-
-
+  | [] -> failwith "unexpected empty list"
   | RPAREN :: _ -> failwith "RPAREN unexpected"
-  | hd :: tl -> (ATOM hd, tl)
-  | [] -> failwith "NOTHING"
+  (*  ( ( + 1 2 3) 2 )  *)
+  | LPAREN :: tl ->
+      let rec sexp_until_rparen acc tl =
+        match tl with
+        | [] -> failwith "unexpected empty list"
+        | RPAREN :: tl -> (List (List.rev acc), tl)
+        | _ ->
+            let (sexp, tl') = sexp_of_lexeme_list tl in
+            sexp_until_rparen (sexp :: acc) tl'
+      in
+      sexp_until_rparen [] tl
 
-(* (* 4 2)*)
-  | [] -> 
+      (* let (sexp, tl') = sexp_of_lexeme_list tl in *)
+      (* let (sexp', tl'') = sexp_of_lexeme_list tl' in *)
+      (* (List [sexp; sexp'], tl'' (without the RPAREN)) *)
+
+  | hd :: tl -> (Atom hd, tl)
+
+(* TODO: parse function should call [sexp_of_lexeme_list], but just
+ check that the remaining [lexeme list] is empty and return the [sexp] *)
+let parse = failwith "TODO"
+
+(* TODO: Try converting sexps to ASTs! *)
+(* TODO: Write a [string_of_sexp], which will need a [string_of_lexeme] *)
+(* TODO: Write some simple example inputs and print outputs *)
+
