d780967d2d389ed134a25a238a91b275771d26ab
Eric Lee <me@ericchanlee.com>
2026-04-27 10:04:28 -0700

    feat: patch lexer infinite loop

--- test/test_crafting_interpreters.ml
+++ test/test_crafting_interpreters.ml
@@ -1,15 +1,4 @@
 open Crafting_interpreters.Lexer
 
-(* 3 + 2 + 1
- [NUMBER 3, PLUS, NUMBER 2, PLUS, NUMBER 1]
- *)
-
-let () = 
-  assert(lex ("1 + 2 + 3") = [NUMBER 43949; PLUS; NUMBER 2; PLUS; NUMBER 1]);
-  (* assert(sexp_of_lexeme_list [NUMBER 1; PLUS; NUMBER 2] = List [Atom (NUMBER 1); Atom PLUS; Atom (NUMBER 2)]); *)
-
-
-
-(* let%expect_test "test example" = *)
-(*   print_endline "hello"; *)
-(*   [%expect "hello"] *)
+let () = assert (lex "1 + 2 + 3" = [ NUMBER 1; PLUS; NUMBER 2; PLUS; NUMBER 3 ])
+(* assert(sexp_of_lexeme_list [NUMBER 1; PLUS; NUMBER 2] = List [Atom (NUMBER 1); Atom PLUS; Atom (NUMBER 2)]); *)
